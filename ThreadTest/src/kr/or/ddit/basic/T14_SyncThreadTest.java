0ae7f8d50b577499fa0435328fb52e2fb632dd5e
duhwan1001 <79068555+duhwan1001@users.noreply.github.com>
2021-03-25 17:16:33 +0900

    03.25

---
+++ ThreadTest/src/kr/or/ddit/basic/T14_SyncThreadTest.java
@@ -0,0 +1,60 @@
+package kr.or.ddit.basic;
+
+public class T14_SyncThreadTest {
+	
+	public static void main(String[] args) {
+		
+		ShareObject sObj = new ShareObject();
+		
+		WorkerThread th1 = new WorkerThread("1번쓰레드", sObj);
+		WorkerThread th2 = new WorkerThread("2번쓰레드", sObj);
+		
+		th1.start();
+		th2.start();
+		
+		
+	}
+}
+
+// 공통으로 사용할 객체
+
+class ShareObject {
+	private int sum = 0;
+	
+	// 동기화 하는 방법1 : 메서드 자체에 동기화 설정하기
+	public void add() { // public synchronized void
+		// 동기화 하는 방법2 : 동기화 블럭으로 설정하기
+		synchronized (this) { // 동기화는 꼭 필요한곳에만 지정해준다. == 느려지기 때문
+		for(int i=0; i<1000000000; i++) {} // 동기화 처리 전까지의 시간벌기용 
+		
+			int n = sum;
+			n += 10; // 10 증가
+			sum = n;
+		
+		System.out.println(Thread.currentThread().getName()
+						+ "합계 : " + sum);
+		}
+	}
+	
+	public int getSum() {
+		return sum;
+	}
+}
+
+// 작업을 수행하는 스레드 클래스
+class WorkerThread extends Thread {
+	ShareObject sObj;
+	
+	public WorkerThread(String name, ShareObject sObj) {
+		super(name);
+		this.sObj = sObj;
+	}
+	
+	@Override
+	public void run() {
+		for(int i=1; i<=10; i++) {
+			sObj.add();
+		}
+		super.run();
+	}
+}
---
+++ ThreadTest/src/kr/or/ddit/basic/T15_SyncAccountTest.java
@@ -0,0 +1,85 @@
+package kr.or.ddit.basic;
+/**
+ * 은행의 입출금을 스레드로 처리하는 예제
+ * (synchronized를 이용한 동기화 처리)
+ */
+public class T15_SyncAccountTest {
+	public static void main(String[] args) {
+		
+		SyncAccount sAcc = new SyncAccount();
+		sAcc.setBalance(20000); // 입금처리
+		
+		BankThread bth1 = new BankThread(sAcc);
+		BankThread bth2 = new BankThread(sAcc);
+		
+		bth1.start();
+		bth2.start();
+			
+		// 동기화 하지않고 실행했을때 결과 값 : -2000
+		// 동시에 서로 같은 작업을 했기때문에 10000원에서 12000원을 인출한것이라고 이해하면 됨
+	}
+}
+
+//은행의 입출금을 관리하는 클래스 정의(공통 객체)
+class SyncAccount {
+	private int balance; // 잔액이 저장될 변수
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	// 입금 처리를 수행하는 메서드
+	public void deposit(int money) {
+		balance += money;
+	}
+	
+	// 출금을 처리하는 메서드(출금 성공: true, 출금 실패: false 반환)
+	// 동기화 영역에서 호출하는 메서드도 동기화 처리를 해 주어야 한다.
+	
+	synchronized public boolean withdraw(int money) {
+//		synchronized (this) {
+			if(balance >= money) { // 잔액이 충분할 경우..
+				for(int i=0; i<= 100000000; i++) {} // 시간 때우기
+				
+				balance -= money;
+				System.out.println("메서드 안에서 balance = " + getBalance());
+				
+				return true;
+			} else {
+				return false;
+			}
+//		}
+	}
+}
+
+// 은행 업무를 처리하는 스레드
+class BankThread extends Thread {
+	private SyncAccount sAcc;
+	
+	public BankThread(SyncAccount sAcc) {
+		this.sAcc = sAcc;
+	}
+
+	@Override
+	public void run() {
+		boolean result = sAcc.withdraw(6000); //6000원 인출
+		System.out.println("스레드 안에서 result = " + result
+							+ ", balance = " + sAcc.getBalance());
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
