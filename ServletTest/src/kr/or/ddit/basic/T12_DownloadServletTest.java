1fed80c09960757276d24228ca093507d9aca1e1
duhwan1001 <79068555+duhwan1001@users.noreply.github.com>
2021-04-08 17:21:26 +0900

    04.08

---
+++ ServletTest/src/kr/or/ddit/basic/T12_DownloadServletTest.java
@@ -0,0 +1,81 @@
+package kr.or.ddit.basic;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class T12_DownloadServletTest extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String filename = "blue.jpg";
+		
+		// 파일 다운롤드 처리를 위한 응답헤더 정보 설정하기
+		resp.setContentType("application/octet-stream");
+		resp.setHeader("Content-Disposition","attachment; filename=\""
+													+ filename + "\"");
+		
+		resp.setContentType("image/jpg");
+		
+		ServletOutputStream out = resp.getOutputStream();
+		
+		FileInputStream fis = new FileInputStream("d:/D_Other/blue.jpg");
+		
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		BufferedOutputStream bos = new BufferedOutputStream(out);
+		
+		int bytes = 0; // 읽은 바이트 수
+		while((bytes = bis.read()) != -1) {
+			bos.write(bytes);
+		}
+		
+		bis.close();
+		bos.close();
+		/**
+		 * Content-Disposition 헤더에 대하여
+		 * 
+		 * 1. response header 에서 사용되는 경우... ex) 파일다운로드
+		 * 
+		 * 		Content-Disposition: inline (default)
+		 *		// 파일 다운로드
+		 * 		Content-Disposition: attachment
+		 * 		// filename.jpg 이름으로 파일 다운로드
+		 * 		Content-Disposition: attachment; filename="filename.jpg" // 원하는 이름으로 받음
+		 * 
+		 * 2. multipart body를 위한 헤더 정보로 사용되는 경우... ex) 파일 업로드
+		 * 		
+		 * 		Content-Disposition: form-data
+		 * 		Content-Disposition: form-data; name="fieldName"
+		 * 		Content-Disposition: form-data; name="fieldName"; filename="filename.jpg"
+		 * 
+		 */
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
